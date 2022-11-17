aae9fd0f41dbb843519864dee7e93740d59d91b4
Emanuele Paolini <emanuele.paolini@gmail.com>
2022-11-17 17:31:54 +0100

    rimossa query "public" non più utilizzata

--- server/controllers/VisitController.js
+++ server/controllers/VisitController.js
@@ -9,43 +9,6 @@ class VisitController extends Controller {
         this.managerRoles.push('visit-manager')
         this.supervisorRoles.push('visit-manager', 'visit-supervisor')
     }
-
-    async public(req, res) {
-        try {
-            let today = new Date()
-            let tomorrow = today
-            tomorrow.setDate(today.getDate() + 1)
-            let visits = await Visit.find({
-                startDate: {$lte: tomorrow},
-                endDate: {$gte: today}
-            })
-            const data = visits.map(visit => ({
-                startDate: visit.startDate,
-                endDate: visit.endDate,
-                firstName: visit.firstName,
-                lastName: visit.lastName,
-                affiliation: visit.affiliation,
-                roomNumber: visit.roomNumber,
-                building: visit.building,               
-            }))
-            res.send({
-                data,
-                visits: data // backward compatibility
-            })
-        } catch(err) {
-            res.status("500")
-            res.send({ error: err.message })
-        }
-    }
-
-    register(router) {
-        let paths = super.register(router)
-
-        paths.push(`get /public/${this.path}`)
-        router.get(`/public/${this.path}/`, 
-            (req, res) => this.public(req,res))
-        return paths
-    }
 }
 
 module.exports = VisitController
