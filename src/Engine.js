7dc978228411c01cdfbbf11b26919246decee925
Emanuele Paolini <emanuele.paolini@gmail.com>
2022-12-02 21:21:08 +0100

    fix useEffect dependencies

--- src/Engine.js
+++ src/Engine.js
@@ -250,7 +250,7 @@ export function useCreateEngine() {
                         ))
                     })
                 })
-            }, [])
+            }, [_id, related])
             return data
         }
     }
