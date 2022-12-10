a9aa972f9859d8ce226f2b4a0174ece1e67df7df
Emanuele Paolini <emanuele.paolini@gmail.com>
2022-12-10 22:56:57 +0100

    fix unused var

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import { NavLink } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 
 import { useEngine } from '../Engine'
