39e7ad321c764290c2df1c4ad16a0ca32f0272d2
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-01-17 11:49:32 +0100

    Fix Grant population

--- src/pages/VisitDetails.js
+++ src/pages/VisitDetails.js
@@ -1,12 +1,17 @@
+import { useEngine } from '../Engine'
 import PersonDetails from './PersonDetails'
 
 export default function VisitDetails({obj}) {
+    const engine = useEngine()
     const visit = obj
     const person = visit.person
     if (visit.person === null) return
-    return <>
-        <p><b>persona:</b> <a href={`/person/${person._id}`}>{person.lastName}, {person.firstName}</a></p>
-        <PersonDetails obj={visit.person} />
-    </>
+    let elements = []
+    const Person = engine.Models.Person
+    if (engine.user.hasSomeRole(...Person.schema.supervisorRoles)) {
+        elements.push(<p><b>persona:</b> <a href={`/person/${person._id}`}>{person.lastName}, {person.firstName}</a></p>)
+        elements.push(<PersonDetails obj={visit.person} />)
+    }
+    return elements
 }
 
