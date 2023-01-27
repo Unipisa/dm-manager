da48a0b5dcde20378c3af68a2ce86863a75fddcf
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-01-27 17:27:01 +0100

    aggiunti gruppi

---
+++ src/models/Group.js
@@ -0,0 +1,28 @@
+import Model from './Model'
+
+export default class Group extends Model {
+    constructor() {
+        super()
+        this.code = 'group'
+        this.name = "gruppo"
+        this.oa = "o"
+        this.articulation = {
+            'oggetto': "gruppo", 
+            'oggetti': "gruppi",
+            'l\'oggetto': "il gruppo",
+            'gli oggetti': "i gruppi", 
+            'un oggetto': "un gruppo", 
+        }
+        this.ModelName = 'Group' 
+        this.columns = {
+            'name': "nome",
+            'startDate': "dal",
+            'endDate': "al",
+            'members': "membri",
+            'updatedAt': "modificato",
+        }
+    }
+
+    describe(obj) { return `${obj?.name}` }
+}
+
