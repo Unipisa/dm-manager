43333e890a4a358c98eb31a82e458ee679f2d383
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-02-05 18:22:54 +0100

    ricerca stanza tramite nome occupante

--- server/controllers/RoomController.js
+++ server/controllers/RoomController.js
@@ -8,7 +8,7 @@ class RoomController extends Controller {
         this.path = 'room'
         this.managerRoles.push('room-manager')
         this.supervisorRoles.push('room-manager', 'room-supervisor', 'assignment-manager', 'assignment-supervisor')
-        this.searchFields = ['code', 'notes']
+        this.searchFields = ['code', 'notes', 'roomAssignments.person.lastName', 'roomAssignments.person.firstName']
         this.searchRoles = ['room-manager', 'room-supervisor', 'assignment-manager']
 
         // inserisce le assegnazioni
