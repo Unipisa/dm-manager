b4fcbc0011e69d556f261355341d5e40dfd7eada
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-02-08 17:35:05 +0100

    FormData

--- server/controllers/FormController.js
+++ server/controllers/FormController.js
@@ -1,4 +1,5 @@
 const Form = require('../models/Form')
+const FormData = require('../models/FormData')
 const Controller = require('./Controller')
 
 class FormController extends Controller {
@@ -10,10 +11,29 @@ class FormController extends Controller {
         this.searchFields = [ 'name', 'text' ]
     }
 
-    putFill(req, res) {
+    async putFill(req, res) {
         const id = req.params.id
         const user = req.user
-        const form = this.Model.findById(id)
+        const form = await this.Model.findById(id)
+        if (!form) return res.status(404).send({error: 'Form not found'})
+        const map = new Map()
+        Object.entries(req.body).forEach(([key, value]) => {
+            map.set(key, value)
+        })
+        try {
+            const data = new FormData({
+                data: map,
+                email: user.email || null,
+                lastName: user.lastName || null,
+                firstName: user.firstName || null,
+                createdBy: user._id,
+                updatedBy: user._id,
+            })
+            const obj = await data.save()
+            return res.send(obj)
+        } catch (err) {
+            return res.status(400).send({error: err.message})
+        }
     }
 
     register(router) {
--- src/pages/FormFillPage.js
+++ src/pages/FormFillPage.js
@@ -1,5 +1,9 @@
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
+import { useEngine } from '../Engine'
+import api from '../api'
 import Models from '../models/Models'
 import { useObject, ObjectProvider } from '../components/ObjectProvider'
 
@@ -12,28 +16,72 @@ export default function FormFillPage() {
 }
 
 function FormFillPageInner() {
+    const engine = useEngine()
+    const user = engine.user
+    const vars = {
+        email: user.email,
+        username: user.username,
+        lastName: user.lastName,
+        firstName: user.firstName,
+    }
+    const [data, setData] = useState({})
     const form = useObject()
+    const [thanks, setThanks] = useState(false)
+
+    async function submit() {
+        const res = await api.put(`${Models.Form.viewUrl(form._id)}/fill`, data)
+        setThanks(true)
+    }
+
+    if (thanks) return <p>
+        <b>Grazie per aver compilato il modulo!</b>
+        <br />
+        Le tue risposte sono state inviate, 
+        puoi chiudere questa pagina.</p>
+
     return <>
-        { renderHtml(form.text) }
+        <h1>{form.name}</h1>
+        <RenderHtml text={form.text} vars={vars} 
+            data={data} setData={setData} />
+        <Button onClick={submit}>invia modulo</Button>
     </>
+
 }
 
-function renderHtml(html) {
+function RenderHtml({text, vars, data, setData}) {
     const parser = new DOMParser()
-    const doc = parser.parseFromString(html, 'text/html')
-    console.log('doc')
-    console.dir(doc)
-    return renderElement(doc.body, 'body')
+    const doc = parser.parseFromString(text, 'text/html')
+    return <RenderElement el={doc.body} key="body"
+         vars={vars} data={data} setData={setData} />
 }
 
-function renderElement(el, key) {
+function Error({key, children}) {
+    return <span key={key} className='text-danger bg-warning'>{'<'}{children}{'>'}</span>
+}
+
+function RenderElement({el, key, vars, data, setData}) {
 const nodeName = el.nodeName.toLowerCase()
-console.log(`rendering ${nodeName} ${el}`)
-console.dir(el)
 if (nodeName === '#text') return el.textContent
-const children = [...el.childNodes].map((child, i) => renderElement(child,i))
+const children = [...el.childNodes].map((child, i) => 
+    <RenderElement el={child} key={i} vars={vars} 
+        data={data} setData={setData} />)
+//return <>({nodeName}){children}(/{nodeName})</>
+if (nodeName == 'var') {
+    if (!vars) return <Error key={key}>internal error: no vars</Error>
+    const varName = el.textContent
+    if (!vars[varName]) return <Error key={key}>invalid var: {`"${varName}"`}</Error>
+    return <span key={key}>{vars[varName]}</span>
+}
+if (nodeName === 'br') return <br key={key} />
+if (nodeName === 'p') return <p key={key}>{children}</p>
 if (nodeName === 'b') return <b key={key}>{children}</b>
-if (nodeName === 'select') return <select name={el.name} value={el.value}>{children}</select>
+if (nodeName === 'i') return <i key={key}>{children}</i>
+if (nodeName === 'select') return <select 
+    name={el.name} value={data[el.name]}
+    onChange={ evt => setData(data => ({
+        ...data, [el.name]: evt.target.value}))}>
+    {children}
+</select>
 if (nodeName === 'option') return <option value={el.value}>{children}</option>
 return <>{children}</>
 }
