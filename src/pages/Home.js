0005ff25c15aa048fa16bc1391ab8fb192f95378
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-02-11 08:52:38 +0100

    redirect after login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
+import { Navigate } from 'react-router-dom'
+
 import { useEngine } from '../Engine'
 import Models from '../models/Models'
 
 export default function Home() {
   const engine = useEngine()
   const user = engine.user
+
+  const redirect_after_login = sessionStorage.getItem("redirect_after_login")
+  if (redirect_after_login) {
+      sessionStorage.removeItem("redirect_after_login")
+      //sessionStorage.setItem("redirect_after_login", 'fatto')
+      return <Navigate to={redirect_after_login} />
+  }
+  
 
   const modelElements = Object.values(Models)
     .filter(Model => Model.HomeElement !== null)
