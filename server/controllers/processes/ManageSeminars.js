72914173c9f3a93c7220c7f5a4b25d43b3d0ff13
Leonardo Robol <leo@robol.it>
2023-11-15 15:11:15 +0100

    Delete button.

--- server/controllers/processes/ManageSeminars.js
+++ server/controllers/processes/ManageSeminars.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const EventSeminarController = require('../EventSeminarController')
+const EventSeminar = require('../../models/EventSeminar')
+
+const controller = new EventSeminarController()
 
 router.get('/', async (req, res) => {    
-    const controller = new EventSeminarController()
-
     if (req.user === undefined) {
         res.status(401).json({
             result: "Unauthorized"
@@ -15,6 +16,20 @@ router.get('/', async (req, res) => {
         controller.performQuery({ createdBy: req.user._id }, res)
     }
 
+})
+
+router.delete('/:id', async (req, res) => {
+    const seminar = await EventSeminar.findById(req.params.id)
+
+    if (req.user.equals(seminar.createdBy)) {
+        await seminar.delete()
+        res.json({})
+    }
+    else {
+        res.status(401).json({
+            error: "Cannot delete seminars created by other users"
+        })
+    }
 })
 
 module.exports = router
--- src/processes/ManageSeminars.js
+++ src/processes/ManageSeminars.js
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 import { Card } from 'react-bootstrap'
+import axios from 'axios'
 
 const queryClient = new QueryClient()
 
@@ -25,21 +26,35 @@ function SeminarList() {
         return "Error: " + error.message
     }
 
+    const deleteSeminar = async (id) => {
+        try {
+            await axios.delete("/api/v0/process/seminars/" + id)
+        }
+        catch {
+            console.log("Error while deleting the seminar")
+        }
+        
+        queryClient.invalidateQueries('process/seminars')
+    }
+
     var seminar_block = []
 
     for (var i = 0; i < data.data.length; i++) {
         const seminar = data.data[i]
         const speaker = seminar.speaker
         seminar_block.push(
-            <div className="p-3 col-lg-6 p-0">
-            <Card className="shadow" key={"seminar-" + seminar._id}>
+            <div className="p-3 col-lg-6 p-0" key={"seminar-" + seminar._id}>
+            <Card className="shadow">
                 <Card.Header className="h6">Seminario</Card.Header>
                 <Card.Body>
                     <strong>Titolo</strong>: {seminar.title} <br></br>
                     <strong>Speaker</strong>: {speaker.firstName} { speaker.lastName } ({speaker.affiliations.map(x => x.name).join(", ")})<br></br>
                     <strong>Data</strong>: {seminar.startDatetime}<br></br>
-                    <div className="mt-2 d-flex flex-row justify-content-end">
-                        <a href={"/process/seminars/add/" + seminar._id}>
+                    <div className="mt-2 d-flex flex-row justify-content-end">                        
+                        <button className="ms-2 btn btn-danger" onClick={() => deleteSeminar(seminar._id)}>
+                            Delete
+                        </button>
+                        <a className="ms-2" href={"/process/seminars/add/" + seminar._id}>
                             <button className="btn btn-primary">
                                 Edit
                             </button>
