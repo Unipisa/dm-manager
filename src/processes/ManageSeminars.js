d3f009df1c50ec429f462b61041c8173376bda50
Leonardo Robol <leo@robol.it>
2023-11-21 08:26:34 +0100

    Do not import unused QueryClient and QueryClientProvider.

--- src/processes/ManageSeminars.js
+++ src/processes/ManageSeminars.js
@@ -1,4 +1,4 @@
-import { QueryClient, QueryClientProvider, useQuery, useQueryClient } from 'react-query'
+import { useQuery, useQueryClient } from 'react-query'
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import api from '../api'
