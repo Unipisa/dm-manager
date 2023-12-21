e1cd0cbffb7be1d800f796d750743a821fd66fe0
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-12-21 10:41:03 +0100

    fix compilation and version bump

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import { Card } from 'react-bootstrap'
 
 import { useEngine } from '../Engine'
-import Models from '../models/Models'
+// import Models from '../models/Models'
 
 export default function Home() {
   const engine = useEngine()
@@ -80,7 +80,7 @@ export default function Home() {
         Puoi accedere al <a href="/profile">tuo profilo</a>.
         Per problemi o informazioni scrivi a <a href="mailto:help@dm.unipi.it">help@dm.unipi.it</a>.
         {user.roles.length>0 && `I tuoi permessi: ${user.roles.join(', ')}. `}
-        {user.roles.length==0 && `Non risultano permessi assegnati al tuo utente.`}
+        {user.roles.length===0 && `Non risultano permessi assegnati al tuo utente.`}
     </p>
   </>
   }
