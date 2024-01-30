51d6e98d9fe0c95eda2252c433b598757fbbd06d
cdp-21 <132059856+cdp-21@users.noreply.github.com>
2024-01-30 10:09:41 +0000

    tidy up code

--- widgets/src/components/HomeEventList.js
+++ widgets/src/components/HomeEventList.js
@@ -5,9 +5,9 @@ import { Loading } from './Loading';
 import axios from 'axios';
 import { formatDateInterval, getManageURL, getDMURL, formatDate, formatTime, truncateText, truncateTextByWords, isEnglish } from '../utils';
 import './styles.css';
-import Markdown from 'react-markdown'
-import rehypeKatex from 'rehype-katex'
-import remarkMath from 'remark-math'
+import Markdown from 'react-markdown';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 export function HomeEventList({}) {
     const [numberOfEntries, setNumberOfEntries] = useState(3);
@@ -16,7 +16,7 @@ export function HomeEventList({}) {
         var events = []
         const now = new Date()
 
-        const conf = await axios.get(getManageURL("public/conferences"), { params: { _limit: numberOfEntries, _sort: "startDate", from: now} })
+        const conf = await axios.get(getManageURL("public/conferences"), { params: { _limit: numberOfEntries, _sort: "startDate", from: "2023-01-01"} })
         if (conf.data) {
             const ec = conf.data.data              
             const ec_label =  ec.map(x => { 
@@ -25,7 +25,7 @@ export function HomeEventList({}) {
             events.push(...ec_label)
         }
 
-        const sem = await axios.get(getManageURL("public/seminars"), { params: { _limit: numberOfEntries, _sort: "startDatetime", from: now} })
+        const sem = await axios.get(getManageURL("public/seminars"), { params: { _limit: numberOfEntries, _sort: "startDatetime", from: "2023-01-01"} })
         if (sem.data) {
             const es = sem.data.data
             const es_label = es.map(x => { 
@@ -47,101 +47,101 @@ export function HomeEventList({}) {
         return <Loading widget="Lista degli eventi" error={error}></Loading>
     }
 
-    const all_event_list = data.map((x) => {
-        return <EventBox event={x} key={x._id}></EventBox>
-    })
+    const all_event_list = data.map((x) => (
+        <EventBox event={x} key={x._id}></EventBox>
+      ));
+    
+      const seminar_list = filterEventsByType(data, 'seminar').map((seminar) => (
+        <EventBox event={seminar} key={seminar._id}></EventBox>
+      ));
+    
+      const conference_list = filterEventsByType(data, 'conference').map(
+        (conference) => (
+          <EventBox event={conference} key={conference._id}></EventBox>
+        )
+      );
+    
+      const colloquia_list = filterEventsByCategory(data, 'Colloquium').map(
+        (colloquium) => (
+          <EventBox event={colloquium} key={colloquium._id}></EventBox>
+        )
+      );
 
-    const seminars = data.filter((event) => event.type === 'seminar');
-    const conferences = data.filter((event) => event.type === 'conference');
-    const colloquia = data.filter((event) => event.category?.name === 'Colloquium');
-
-    const seminar_list = seminars.map((seminar) => {
-        return <EventBox event={seminar} key={seminar._id}></EventBox>;
-    });
-    const conference_list = conferences.map((conference) => {
-        return <EventBox event={conference} key={conference._id}></EventBox>;
-    });
-    const colloquia_list = colloquia.map((colloquium) => {
-        return <EventBox event={colloquium} key={colloquium._id}></EventBox>;
-    });
-
-    return <div className="">
-        <Tab.Container id="left-tabs-example" defaultActiveKey="all">
-          <Nav variant="pills" className="flex-row d-flex justify-content-center">
-            <Nav.Item>
-              <Nav.Link eventKey="all" className="filter-link">
-                {isEnglish() ? "All" : "Tutti"}
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="conferences" className="filter-link">
-                {isEnglish() ? "Conferences" : "Conferenze"}
-              </Nav.Link>
-            </Nav.Item>           
-            <Nav.Item>
-              <Nav.Link eventKey="seminars" className="filter-link">
-                {isEnglish() ? "Seminars" : "Seminari"}
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="colloquia" className="filter-link">Colloquia</Nav.Link>
-            </Nav.Item>
-          </Nav>
-          <Tab.Content>
-            <Tab.Pane eventKey="all">
-                <div className="row">
-                    {all_event_list}
-                </div>
-            </Tab.Pane>
-            <Tab.Pane eventKey="conferences">
-                <div className="row">
-                    {conference_list}    
-                </div>
-            </Tab.Pane>
-            <Tab.Pane eventKey="seminars">
-                <div className="row">
-                    {seminar_list}    
-                </div>
-            </Tab.Pane>
-            <Tab.Pane eventKey="colloquia">
-                <div className="row">
-                    {colloquia_list}    
-                </div>
-            </Tab.Pane>
-          </Tab.Content>
-        </Tab.Container>
-        <div className="d-flex flex-row justify-content-center">
-            <Button onClick={x => {setNumberOfEntries(numberOfEntries + 3)}}>
-                {isEnglish() ? "Load more" : "Carica altro"}
-            </Button></div>
-    </div>
+      return (
+        <div className="">
+          <Tab.Container id="left-tabs-example" defaultActiveKey="all">
+            <Nav variant="pills" className="flex-row d-flex justify-content-center">
+              <Nav.Item>
+                <Nav.Link eventKey="all" className="filter-link">
+                  {isEnglish() ? 'All' : 'Tutti'}
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="conferences" className="filter-link">
+                  {isEnglish() ? 'Conferences' : 'Conferenze'}
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="seminars" className="filter-link">
+                  {isEnglish() ? 'Seminars' : 'Seminari'}
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="colloquia" className="filter-link">
+                  Colloquia
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <Tab.Content>
+              <Tab.Pane eventKey="all">
+                <div className="row">{all_event_list}</div>
+              </Tab.Pane>
+              <Tab.Pane eventKey="conferences">
+                <div className="row">{conference_list}</div>
+              </Tab.Pane>
+              <Tab.Pane eventKey="seminars">
+                <div className="row">{seminar_list}</div>
+              </Tab.Pane>
+              <Tab.Pane eventKey="colloquia">
+                <div className="row">{colloquia_list}</div>
+              </Tab.Pane>
+            </Tab.Content>
+          </Tab.Container>
+          <div className="d-flex flex-row justify-content-center">
+            <Button onClick={() => setNumberOfEntries(numberOfEntries + 3)}>
+              {isEnglish() ? 'Load more' : 'Carica altro'}
+            </Button>
+          </div>
+        </div>
+      );
 }
 
-function EventBox({ event }) {
-    var date = undefined;
-    var link = undefined;
+function filterEventsByType(events, type) {
+    return events.filter((event) => event.type === type);
+  }
+  
+  function filterEventsByCategory(events, category) {
+    return events.filter((event) => event.category?.name === category);
+  }
 
-    if (event.endDate) {
-        date = formatDateInterval(event.startDate, event.endDate)
-    }
-    else {
-        date = formatDate(event.startDatetime) + " - " + formatTime((event.startDatetime))
-    }
+function EventBox({ event }) {    
+    const date = event.endDate
+    ? formatDateInterval(event.startDate, event.endDate)
+    : `${formatDate(event.startDatetime)} - ${formatTime(event.startDatetime)}`;
 
-    if (event.type == 'seminar') {
-        link = getDMURL("en/seminar?id=" + event._id)
-    } 
-    else {
-        link = getDMURL("en/conference?id=" + event._id)
-    }
+    const link = event.type === 'seminar' ? getDMURL(`en/seminar?id=${event._id}`) : getDMURL(`en/conference?id=${event._id}`);
 
     return <div className="col-12 col-md-6 col-lg-4 mb-4 p-4" style={{ width: "300px", height: "300px", overflow: "hidden" }}>
         <h2 className="mb-1 title_style">
-            <a href={link} className="title_style">{truncateTextByWords(event.title, 20)}</a>
+            <a href={link} className="title_style">
+                {truncateTextByWords(event.title, 20)}
+            </a>
         </h2>
         <div className="date_style far fa-calendar"> {date}</div>
         <div className="excerpt_style">
-            <Markdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>{truncateTextByWords(event.abstract ? event.abstract : event.description, 40)}</Markdown>
+            <Markdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
+                {truncateTextByWords(event.abstract ? event.abstract : event.description, 40)}
+            </Markdown>
         </div>
     </div>
 }
