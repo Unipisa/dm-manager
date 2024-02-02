5db801bb95fcb00e6be5ebde32ca01c3a82949b9
Emanuele Paolini <emanuele.paolini@gmail.com>
2024-02-02 17:43:05 +0100

    better visit notification message

--- server/command.js
+++ server/command.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const {setupDatabase} = require('./database')
 const migrations = require('./migrations')
+const {notifyVisit} = require('./controllers/processes/visits')
 
 async function main() {
     await setupDatabase()
@@ -20,33 +21,45 @@ available command line options:
  show-migrations: show migrations state
  apply-migrations: apply pending migrations
  clean-migrations: clean removed migrations
+ notify-visit <visit_id> [<message>]: send visit notification message
 
     `)
 
     console.log(`given command line arguments: ${JSON.stringify(process.argv.slice(2))}`)
 
-    for (let arg of process.argv.slice(2)) {
-        if (arg === 'worker') {
-            
-        } else if (arg === 'clear-sessions' || arg === '-c') {
-            console.log('* clear sessions collection')
-            await mongoose.connection.db.collection('sessions').deleteMany({})
+    const command = process.argv[2]
+    const args = process.argv.slice(3)
 
-        } else if (arg === 'show-migrations') {
-            console.log('* show migrations state')
-            await migrations.migrate(mongoose.connection.db, {})
+    if (command === 'worker') {
+        require('./worker') // non funziona
+    } else if (command === 'clear-sessions' || command === '-c') {
+        console.log('* clear sessions collection')
+        await mongoose.connection.db.collection('sessions').deleteMany({})
 
-        } else if (arg === 'apply-migrations') {
-            console.log('* apply pending migrations')
-            await migrations.migrate(mongoose.connection.db, {apply: true})
+    } else if (command === 'show-migrations') {
+        console.log('* show migrations state')
+        await migrations.migrate(mongoose.connection.db, {})
 
-        } else if (arg === 'clean-migrations') {
-            console.log('* clean migrations collection')
-            await migrations.migrate(mongoose.connection.db, {clean: true})
+    } else if (command === 'apply-migrations') {
+        console.log('* apply pending migrations')
+        await migrations.migrate(mongoose.connection.db, {apply: true})
 
-        } else {
-            console.log(`invalid argument: ${arg}`)
+    } else if (command === 'clean-migrations') {
+        console.log('* clean migrations collection')
+        await migrations.migrate(mongoose.connection.db, {clean: true})
+
+    } else if (command === 'notify-visit') {
+        const visit_id = args[0]
+        const message = args[1] || ''
+        console.log(`* notify visit ${visit_id} with message: ${message}`)
+        if (!visit_id) {
+            console.log('<visit_id> required')
+            return
         }
+        await notifyVisit(visit_id, message)
+    
+    } else {
+        console.log(`invalid argument: ${arg}`)
     }
 }
 
