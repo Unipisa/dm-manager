acc6ff2aaff8b2dc76328d8a9859a9aa6aac37eb
Emanuele Paolini <emanuele.paolini@gmail.com>
2024-02-21 19:24:30 +0100

    apply migration: update past conferences

--- server/migrations.js
+++ server/migrations.js
@@ -509,8 +509,7 @@ const migrations = {
     },
 
        // Load server/past-conferences.json into eventconferences
-       D20240212_update_past_conferences_1: async function (db) {
-        return true
+       D20240212_update_past_conferences_2: async function (db) {
         const conferences = db.collection("eventconferences");
         const conferencerooms = db.collection("conferencerooms");
         const data = require("./past-conferences.json");
