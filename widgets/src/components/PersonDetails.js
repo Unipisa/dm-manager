3d9e84ad5253f66db337d51e598edfb067ecd426
CDP21 <132059856+c2d13p@users.noreply.github.com>
2024-07-11 09:42:36 +0000

    update no image placeholder url

--- widgets/src/components/PersonDetails.js
+++ widgets/src/components/PersonDetails.js
@@ -45,7 +45,7 @@ export function PersonDetails({ person_id }) {
 }
 
 function PersonBlock({data, en}) {
-    const photoUrl = data.photoUrl || "https://www.dm.unipi.it/wp-content/uploads/2022/07/No-Image-Placeholder.svg_.png?resize=280%2C280&ssl=1";
+    const photoUrl = data.photoUrl || "https://www.dm.unipi.it/wp-content/uploads/2024/07/No-Image-Placeholder.png";
     const feminine = data.gender === 'Donna';
     const qualification = (data.staffs || []).map(q => getRoleLabel(q.qualification, en, feminine)).join(', ');
     const researchGroup = [...new Set(data.staffs.map(q => q.SSD).filter(q => q).map(ssd => getResearchGroupLabel(ssd, en)))].join(', ');
