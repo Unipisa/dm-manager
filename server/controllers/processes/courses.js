274c840c4bab0ffaf631141378fa7892d6c45f09
CDP21 <132059856+c2d13p@users.noreply.github.com>
2025-09-24 12:54:37 +0000

    bug fix

--- server/controllers/processes/courses.js
+++ server/controllers/processes/courses.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+module.exports = router
 const { ObjectId } = require('mongoose').Types
 
 const EventPhdCourse = require('../../models/EventPhdCourse')
