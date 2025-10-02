9bc7e8ad49170ee28a07d840c322bd82479188d2
CDP21 <132059856+c2d13p@users.noreply.github.com>
2025-10-02 08:14:37 +0000

    simplify and fix routes for courses

--- server/controllers/processes/courses.js
+++ server/controllers/processes/courses.js
@@ -25,188 +25,11 @@ function pastDate() {
 }
 module.exports.pastDate = pastDate
 
-const INDEX_PIPELINE = [
-    { $lookup: {
-        from: 'people',
-        localField: 'coordinators',
-        foreignField: '_id',
-        as: 'coordinators',
-        pipeline: [
-            { $project: {
-                _id: 1,
-                firstName: 1,
-                lastName: 1,
-                email: 1,
-            }},
-        ]
-    }},
-    { $lookup: {
-        from: 'people',
-        localField: 'lecturers',
-        foreignField: '_id',
-        as: 'lecturers',
-        pipeline: [
-            { $project: {
-                _id: 1,
-                firstName: 1,
-                lastName: 1,
-                email: 1,
-            }},
-        ]
-    }},
-    // Basic lookup for conference rooms in lessons
-    { $addFields: {
-        lessons: {
-            $map: {
-                input: "$lessons",
-                as: "lesson",
-                in: {
-                    $mergeObjects: [
-                        "$$lesson",
-                        { conferenceRoomId: "$$lesson.conferenceRoom" }
-                    ]
-                }
-            }
-        }
-    }},
-]
-module.exports.INDEX_PIPELINE = INDEX_PIPELINE
-
-const GET_PIPELINE = [
-    { $lookup: {
-        from: 'people',
-        localField: 'coordinators',
-        foreignField: '_id',
-        as: 'coordinators',
-        pipeline: [
-            { $lookup: {
-                from: 'institutions',
-                localField: 'affiliations',
-                foreignField: '_id',
-                as: 'affiliations',
-                pipeline: [
-                    { $project: {
-                        _id: 1,
-                        name: 1,
-                    }},
-                ]
-            }},
-            { $project: {
-                _id: 1,
-                firstName: 1,
-                lastName: 1,
-                affiliations: 1,
-                email: 1,
-            }},
-        ]
-    }},
-    { $lookup: {
-        from: 'people',
-        localField: 'lecturers',
-        foreignField: '_id',
-        as: 'lecturers',
-        pipeline: [
-            { $lookup: {
-                from: 'institutions',
-                localField: 'affiliations',
-                foreignField: '_id',
-                as: 'affiliations',
-                pipeline: [
-                    { $project: {
-                        _id: 1,
-                        name: 1,
-                    }},
-                ]
-            }},
-            { $project: {
-                _id: 1,
-                firstName: 1,
-                lastName: 1,
-                affiliations: 1,
-                email: 1,
-            }},
-        ]
-    }},
-    // Expand createdBy
-    {$lookup:{
-        from: "users",
-        localField: "createdBy",
-        foreignField: "_id",
-        as: "createdBy",
-        pipeline: [
-            {$project: {
-                _id: 1,
-                username: 1,
-            }},
-        ],
-    }},
-    {$unwind: {
-        path: "$createdBy",
-        preserveNullAndEmptyArrays: true
-    }},
-    // Enhanced lessons with conference room details
-    { $addFields: {
-        lessons: {
-            $map: {
-                input: "$lessons",
-                as: "lesson",
-                in: {
-                    date: "$$lesson.date",
-                    duration: "$$lesson.duration",
-                    mrbsBookingID: "$$lesson.mrbsBookingID",
-                    conferenceRoomId: "$$lesson.conferenceRoom"
-                }
-            }
-        }
-    }},
-    // Lookup conference rooms for lessons (this is complex due to embedded structure)
-    { $lookup: {
-        from: 'conferencerooms',
-        localField: 'lessons.conferenceRoomId',
-        foreignField: '_id',
-        as: 'conferenceRoomsData'
-    }},
-    // Merge conference room data back into lessons
-    { $addFields: {
-        lessons: {
-            $map: {
-                input: "$lessons",
-                as: "lesson",
-                in: {
-                    $mergeObjects: [
-                        "$$lesson",
-                        {
-                            conferenceRoom: {
-                                $arrayElemAt: [
-                                    {
-                                        $filter: {
-                                            input: "$conferenceRoomsData",
-                                            cond: { $eq: ["$$this._id", "$$lesson.conferenceRoomId"] }
-                                        }
-                                    },
-                                    0
-                                ]
-                            }
-                        }
-                    ]
-                }
-            }
-        }
-    }},
-    // Clean up temporary fields
-    { $unset: ["conferenceRoomsData", "lessons.conferenceRoomId"] }
-]
-module.exports.GET_PIPELINE = GET_PIPELINE
 
 router.get('/', async (req, res) => {
-    let authorization_alternatives = [
-        { createdBy: req.user._id },
-    ]
-    if (req?.person?._id) authorization_alternatives.push({ coordinators: new ObjectId(req.person._id) })
-
     const pipeline = [
         {$match: {
-            $or: authorization_alternatives
+            endDate: { $gte: pastDate() },
         }},
         ...controller.queryPipeline,
     ]
@@ -220,23 +43,10 @@ router.get('/', async (req, res) => {
 router.delete('/:id', async (req, res) => {
     try {
         const course = await EventPhdCourse.findById(new ObjectId(req.params.id))
-        let user_is_coordinator = false
-        if (course.coordinators && course.coordinators.length > 0) {
-            const coordinators = await Person.find({ _id: { $in: course.coordinators }})
-            user_is_coordinator = req.person && coordinators.some(o => o._id.equals(req.person._id))
-        }
-
-        const user_is_creator = req.user.equals(course.createdBy)
-
-        if (user_is_creator || user_is_coordinator) {
-            await course.delete()
-            await log(req, course, {})
-            res.json({})
-        } else {
-            res.status(401).json({
-                error: "Cannot delete courses created by other users or not in the coordinators list"
-            })
-        }
+    
+        await course.delete()
+        await log(req, course, {})
+        res.json({})
     } catch(error) {
         res.status(400).json({
             error: error.message
@@ -245,15 +55,10 @@ router.delete('/:id', async (req, res) => {
 })
 
 router.get('/get/:id', async (req, res) => {
-    let authorization_alternatives = [
-        { createdBy: req.user._id },
-    ]
-    if (req?.person?._id) authorization_alternatives.push({ coordinators: new ObjectId(req.person._id) })
-
     const pipeline = [
         {$match: {
             _id: new ObjectId(req.params.id),
-            $or: authorization_alternatives
+            endDate: { $gte: pastDate() },
         }},
         ...controller.queryPipeline,
     ]
@@ -301,24 +106,7 @@ router.patch('/:id', async (req, res) => {
 
     try {
         const course = await EventPhdCourse.findById(_id)
-        if (course.coordinators && course.coordinators.length > 0) {
-            const coordinators = await Person.find({ _id: { $in: course.coordinators } })
-
-            const user_is_creator = req.user.equals(course.createdBy)
-            const user_is_coordinator = req.person && coordinators.find(o => o._id.equals(req.person._id))
-    
-            if (!user_is_creator && !user_is_coordinator) {
-                res.status(403).json({ error: "Forbidden" })
-                return
-            }
-        } else {
-            const user_is_creator = req.user.equals(course.createdBy)
-            if (!user_is_creator) {
-                res.status(403).json({ error: "Forbidden" })
-                return
-            }
-        }
-        
+ 
         const was = {...course}
         course.set({ ...course, ...payload })
         await course.save()
@@ -344,23 +132,6 @@ router.put('/save', async (req, res) => {
         }
         else {
             const course = await EventPhdCourse.findById(payload._id)
-            if (course.coordinators && course.coordinators.length > 0) {
-                            const coordinators = await Person.find({ _id: { $in: course.coordinators } })
-                            
-                            const user_is_creator = req.user.equals(course.createdBy)
-                            const user_is_coordinator = req.person && coordinators.find(o => o._id.equals(req.person._id))
-                    
-                            if (!user_is_creator && !user_is_coordinator) {
-                                res.status(403).json({ error: "Forbidden" })
-                                return
-                            }
-                        } else {
-                            const user_is_creator = req.user.equals(course.createdBy)
-                            if (!user_is_creator) {
-                                res.status(403).json({ error: "Forbidden" })
-                                return
-                            }
-                        }
             delete payload.createdBy
             
             const was = {...course}
--- server/controllers/processes/coursesMy.js
+++ server/controllers/processes/coursesMy.js
@@ -1,63 +1,80 @@
 const express = require('express')
 const assert = require('assert')
 const { ObjectId } = require('mongoose').Types
-
 const EventPhdCourse = require('../../models/EventPhdCourse')
+const Person = require('../../models/Person')
 const { log } = require('../middleware')
-const { INDEX_PIPELINE, GET_PIPELINE, DAYS_BACK, pastDate } = require('./courses')
+const EventPhdCourseController = require('../EventPhdCourseController')
+const controller = new EventPhdCourseController()
+const { DAYS_BACK, pastDate } = require('./courses')
 
 const router = express.Router()
 module.exports = router
 
 // inject functionality on the current route 
 require('./personSearch')(router)
+require('./conferenceRoomSearch')(router)
+require('./mrbsRoomsBookings')(router)
 
 router.get('/', async (req, res) => {
     const person = req.person
-    if (!person) return res.json({ data: [], person, DAYS_BACK, note: `user ${req.user?.username} has no associated person`})
-
-    const data = await EventPhdCourse.aggregate([
+    if (!person) return res.json({ 
+        data: [], 
+        person,
+        DAYS_BACK,
+        note: `user ${req.user?.username} has no associated person`
+    })
+    
+    const pipeline = [
         { $match: { 
             $or: [
                 { coordinators: person._id },
-                { lecturers: person._id }
+                { lecturers: person._id },
+                { createdBy: req.user._id },
             ],
             endDate: { $gte: pastDate() },
         }},
-        ...INDEX_PIPELINE,
-    ])
-
-    res.json({ data, person, DAYS_BACK })
+        ...controller.queryPipeline,
+    ]
+    
+    const data = await EventPhdCourse.aggregate(pipeline)
+    const processedData = await controller.aggregatePostProcess(data)
+    
+    res.json({ data: processedData, person, DAYS_BACK })
 })
 
 router.delete('/:id', async (req, res) => {
     assert(req.user._id) 
-
     const person = req.person
-    if (!person) return res.status(404).json({ error: `user ${req.user?.username} has no associated person`})
-
-    const course = await EventPhdCourse.findOneAndDelete({
-        _id: new ObjectId(req.params.id),
-        $or: [
-            { coordinators: person._id },
-            { lecturers: person._id }
-        ],
-        endDate: { $gte: pastDate() },
+    if (!person) return res.status(404).json({ 
+        error: `user ${req.user?.username} has no associated person`
     })
-    if (!course) return res.status(404).json({ error: "Course not found" })
-
-    await log(req, course, {})
-
-    res.json({})
+    
+    try {
+        const course = await EventPhdCourse.findOne({
+            _id: new ObjectId(req.params.id),
+            $or: [
+                { coordinators: person._id },
+                { lecturers: person._id },
+                { createdBy: req.user._id },
+            ],
+            endDate: { $gte: pastDate() },
+        })
+        
+        if (!course) return res.status(404).json({ error: "Course not found or not authorized" })
+    } catch(error) {
+        res.status(400).json({ error: error.message })
+    }
 })
 
 router.get('/:id', async (req, res) => {
     assert(req.user._id)
     const person = req.person
-    if (!person) return res.status(404).json({ error: `user ${req.user?.username} has no associated person`})
-
+    if (!person) return res.status(404).json({ 
+        error: `user ${req.user?.username} has no associated person`
+    })
+    
     if (req.params.id === '__new__') {
-        // return empty object
         const course = new EventPhdCourse().toObject()
         course._id = undefined
         return res.json(course)
@@ -70,75 +87,145 @@ router.get('/:id', async (req, res) => {
         return res.status(400).json({ error: `Invalid id: ${req.params.id}` })
     }
     
-    const data = await EventPhdCourse.aggregate([
+    const pipeline = [
         { $match: { 
             _id,
             $or: [
                 { coordinators: person._id },
-                { lecturers: person._id }
+                { lecturers: person._id },
+                { createdBy: req.user._id },
             ],
             endDate: { $gte: pastDate() },
         }},
-        ...GET_PIPELINE,
-    ])
+        ...controller.queryPipeline,
+    ]
     
-    if (data.length === 0) {
-        res.status(404).json({ error: "Not found" })
-        return
+    const data = await EventPhdCourse.aggregate(pipeline)
+    const processedData = await controller.aggregatePostProcess(data)
+    
+    if (processedData.length === 0) {
+        return res.status(404).json({ error: "Not found" })
     }
     
-    res.json({...data[0], user_person: person})
+    res.json({...processedData[0], user_person: person})
 })
 
-router.put('/', async (req, res) => {
-    const payload = {...req.body}
-
+router.post('/', async (req, res) => {
+    let payload = {
+        ...req.body,
+        createdBy: req.user._id,
+        updatedBy: req.user._id,
+    }
     assert(req.user._id)
     const person = req.person
-    if (!person) return res.status(404).json({ error: `user ${req.user?.username} has no associated person`})
-
-    // override fields that user cannot change
-    payload.createdBy = req.user._id
-    payload.updatedBy = req.user._id
+    if (!person) return res.status(404).json({ 
+        error: `user ${req.user?.username} has no associated person`
+    })
+    
     delete payload._id
-
+    
     if (!payload.endDate || new Date(payload.endDate) < pastDate()) {
         return res.status(422).json({ error: `endDate cannot be more than ${DAYS_BACK} days in the past` })
     }
-
-    const course = new EventPhdCourse(payload)
-    await course.save()
-
-    await log(req, {}, payload)
-
-    res.send({_id: course._id})
+    
+    try {
+        const course = EventPhdCourse(payload)
+        await course.save()
+        await log(req, {}, payload)
+        res.send({_id: course._id})
+    } catch (error) {
+        res.status(400).send({ error: error.message })
+    }
 })
 
 router.patch('/:id', async (req, res) => {
     console.log(`PATCH coursesMy ${req.params.id}`)
     const payload = {...req.body}
-
     assert(req.user._id)
     const person = req.person
-    if (!person) return res.status(404).json({ error: `user ${req.user.username} has no associated person`})
-
-    // remove fields that user cannot change
-    delete payload._id
-    delete payload.createdBy
-    payload.updatedBy = req.user._id
-
-    const course = await EventPhdCourse.findOneAndUpdate(
-        {   _id: new ObjectId(req.params.id),
+    if (!person) return res.status(404).json({ 
+        error: `user ${req.user?.username} has no associated person`
+    })
+    
+    try {
+        const course = await EventPhdCourse.findOne({
+            _id: new ObjectId(req.params.id),
             $or: [
                 { coordinators: person._id },
-                { lecturers: person._id }
+                { lecturers: person._id },
+                { createdBy: req.user._id },
             ],
-            endDate: { $gte: pastDate() }}, 
-        payload)
-
-    if (!course) return res.status(404).send({ error: "Not found" })
-    
-    await log(req, course, payload)
-
-    res.send({})
+            endDate: { $gte: pastDate() }
+        })
+        
+        if (!course) return res.status(404).json({ error: "Not found" })
+        
+        // Don't allow changing createdBy
+        delete payload._id
+        delete payload.createdBy
+        payload.updatedBy = req.user._id
+        
+        const was = {...course.toObject()}
+        course.set({ ...course.toObject(), ...payload })
+        await course.save()
+        
+        await log(req, was, payload)
+        res.send({})
+    } catch (error) {
+        res.status(400).send({ error: error.message })
+    }
 })
+
+router.put('/save', async (req, res) => {    
+    let payload = {
+        ...req.body,
+        updatedBy: req.user._id,
+    }
+    assert(req.user._id)
+    const person = req.person
+    if (!person) return res.status(404).json({ 
+        error: `user ${req.user?.username} has no associated person`
+    })
+    
+    try {
+        if (!payload._id) {
+            // Creating new course
+            payload.createdBy = req.user._id
+            delete payload._id
+            
+            if (!payload.endDate || new Date(payload.endDate) < pastDate()) {
+                return res.status(422).json({ error: `endDate cannot be more than ${DAYS_BACK} days in the past` })
+            }
+            
+            const course = EventPhdCourse(payload)
+            await course.save()
+            await log(req, {}, payload)
+            res.send({_id: course._id})
+        } else {
+            // Updating existing course
+            const course = await EventPhdCourse.findOne({
+                _id: payload._id,
+                $or: [
+                    { coordinators: person._id },
+                    { lecturers: person._id },
+                    { createdBy: req.user._id },
+                ],
+                endDate: { $gte: pastDate() }
+            })
+            
+            if (!course) return res.status(404).json({ error: "Not found" })
+            
+            delete payload.createdBy
+            
+            const was = {...course.toObject()}
+            course.set({ ...course.toObject(), ...payload })
+            await course.save()
+            await log(req, was, payload)
+            res.send({})
+        }
+    } catch (error) {
+        res.status(400).send({ error: error.message })
+    }
+})
+
+module.exports = router
