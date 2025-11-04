daf5f65ec1b985694fca025c27d0d2038fa70198
CDP21 <132059856+c2d13p@users.noreply.github.com>
2025-11-04 09:21:31 +0000

    ...and hopefully last one

--- server/controllers/StaffController.js
+++ server/controllers/StaffController.js
@@ -13,38 +13,38 @@ class StaffController extends Controller {
         // inserisce tutte le assegnazioni
         // stanze
         this.queryPipeline.push(...RoomAssignment.personRoomAssignmentPipeline())
-        this.indexPipeline = [
-            {
-                $lookup: {
-                    from: "people",
-                    localField: "person",
-                    foreignField: "_id",
-                    as: "person",
-                    pipeline: [
-                        { $project: { firstName: 1, lastName: 1, affiliations: 1 } },
-                        {
-                            $lookup: {
-                                from: "institutions",
-                                localField: "affiliations",
-                                foreignField: "_id",
-                                as: "affiliations",
-                                pipeline: [
-                                    { $project: { name: 1 } }
-                                ]
-                            }
-                        },
-                        { $sort: { lastName: 1 } },
-                    ]
-                }
-            },
-            {
-                $unwind: {
-                    path: "$person",
-                    preserveNullAndEmptyArrays: true
-                }
-            },
-            ...RoomAssignment.personRoomAssignmentPipeline()
-        ]
+//        this.indexPipeline = [
+//            {
+//                $lookup: {
+//                    from: "people",
+//                    localField: "person",
+//                    foreignField: "_id",
+//                    as: "person",
+//                    pipeline: [
+//                        { $project: { firstName: 1, lastName: 1, affiliations: 1 } },
+//                        {
+//                            $lookup: {
+//                                from: "institutions",
+//                                localField: "affiliations",
+//                                foreignField: "_id",
+//                                as: "affiliations",
+//                                pipeline: [
+//                                    { $project: { name: 1 } }
+//                               ]
+//                            }
+//                        },
+//                        { $sort: { lastName: 1 } },
+//                    ]
+//                }
+//            },
+//            {
+//                $unwind: {
+//                    path: "$person",
+//                   preserveNullAndEmptyArrays: true
+//                }
+//            },
+//           ...RoomAssignment.personRoomAssignmentPipeline()
+//        ]
     }
 }
 
